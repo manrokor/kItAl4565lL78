9a5357aaccc31205158b9d16f591c2f42309a8de
manrokor <53918265+manrokor@users.noreply.github.com>
2019-09-14 00:13:49 +0300

    Add files via upload

---
+++ assets/js/randomTest.js
@@ -0,0 +1,114 @@
+﻿ var DATA_ARRAY0 = [
+
+	data_array1_allPers = [
+["<a><img src='images/monstr1.jpg' alt='Monsters'></a><br>Как зовут хозяина замка в 'Монстрах на каникулах'?","Вова","Мумия","Кирилл","Дракула",4],
+["<a><img src='images/monster-a-12-120.jpg' alt='AB'></a><br>Какая злая птичка всё время взрывалась?","Чак","Чук","Рэд","Бомба",4],
+["<a><img src='images/Angry-4-120.jpg' alt='AB'></a><br>Сколько человек в команде пожарного Сэма?","1","7","122","44",2],
+["<a><img src='images/Sam3.png' alt='Sam'></a><br>Как зовут жёлтого бота","Жук","Оптимус Прайм","Бамблби","Шевроле",3],
+["<a><img src='images/former-a-5-120.jpg' alt='Formers'></a><br>Сколько есть основных серий Звёздных войн?","Три","Две","Сто","Девять",4],
+["<a><img src='images/StrWr_Logo.jpg' alt='Super'></a><br>У какого супергероя есть щит со звездой","Халк","Капитан Америка","Железный человек","Бэтмен",2],
+["<a><img src='images/Marv-120-FPng/Marv-CAm(51)-120.jpg' alt='Marv'></a><br>Посчитаем? 5х5=","10","15","20","25",4],
+	],
+
+      
+	data_array2_AnBi = [
+["<a><img src='images/ABirds-all-120.png' alt='AB'></a><br>Кто вёз торт на день рождения птенца?","Орёл","Лось","Почтальон Печкин","Рэд",4],
+["<a><img src='images/Angry-9-120.jpg' alt='AB'></a><br>Какая злая птичка самая быстрая?","Чак","Чук","Рэд","Бомба",1],
+["<a><img src='images/Angry-2-chuck-120.jpg' alt='AB'></a><br>Какая птичка пописала в озеро умности?","Орёл","Чак","Бомба","Король Свиней",1],
+["<a><img src='images/Angry-11-120.jpg' alt='AB'></a><br>Какую птицу запульнули из рогатки на празднике","Фламинго","Чак","Рэд","Птица-судья",3],
+["<a><img src='images/Angry-6-120.jpg' alt='AB'></a><br>Из чего состоит остров, который нашёл Король свиней?","Камни","Деревья","Песок","Лёд",4],
+["<a><img src='images/Angry-5-120.jpg' alt='AB'></a><br>Для чего свиньи забрали яйца у птиц?","Чтобы бросить их в воду","Чтобы съесть на завтрак","Чтобы закрыть на зиму в погреб","Чтобы поиграть ими в пингпонг",2],
+["<a><img src='images/Angry-13-120.jpg' alt='AB'></a><br>Посчитаем? 6х6=","18","24","30","36",4],
+	],
+
+	data_array3_Monstr = [
+["<a><img src='images/monstr1.jpg' alt='Monsters'></a><br>Сколько главных монстров в мультфильме 'Монстры на каникулах'?","Один","Сто двадцать","Тридцать","Девять",4],
+["<a><img src='images/monster-a-13-120.jpg' alt='Monsters'></a><br>Какой монстр самый желеобразный?","Дракула","Человек-Невидимка","Вольфыч","Желешка",4],
+["<a><img src='images/monster-a-3-120.jpg' alt='Monsters'></a><br>В кого превращается Дракула?","Орёл","Синица","Летучая мышь","Король Свиней",3],
+["<a><img src='images/monster-a-2-120.jpg' alt='Monsters'></a><br>Какой монстр Вольфыч?","Волк","Зомби","Вампир","Волк-оборотень",4],
+["<a><img src='images/monster-a-5-120.png' alt='Monsters'></a><br>Какой монстр всё время разваливается на части?","Желешка","Франкенштейн","Бигфут","Мумия",2],
+["<a><img src='images/monster-a-10-120.png' alt='Monsters'></a><br>Какой сыр ели монстры во второй части мультфильма?","Зелёный","С дырками","Вонючий","Поющий",4],
+["<a><img src='images/monster-a-11-120.jpg' alt='Monsters'></a><br>Посчитаем? 6х6=","18","24","30","36",4],
+	],
+        data_array4_Marvel = [
+["<a><img src='images/Marvel-All-120.jpg' alt='Marvel'></a><br>Как называется красный предмет на голове Йонду?","Хохолок","Чуб","Гребень","Рог",3],
+["<a><img src='images/Marvel-Jondu-120.jpg' alt='Marvel'></a><br>Как звали отца Питера?","Угу","Ого","Эго","Боб",3],
+["<a><img src='images/Marvel-Ego-120.jpg' alt='Marvel'></a><br>Что говорило маленькое дерево?","Я был Грутт","Я есть Грутт","Я тут Грутт","Я Грутт",2],
+["<a><img src='images/Marvel-Derevo1-120.jpg' alt='Marvel'></a><br>Как звали енота?","Волк","Белка","Ракета","Лёша",3],
+["<a><img src='images/Marvel-Enot-120.jpg' alt='Marvel'></a><br>На чем слушал музыку Питер?","Патифон","Магнитофон","Плеер","Телефон",3],
+["<a><img src='images/Marvel-Walkman.jpg' alt='Marvel'></a><br>Какого цвета глаза в маске Питера?","Зелёного","Красного","Коричневого","Синего",2],
+["<a><img src='images/Marvel-Peter-120.jpg' alt='Marvel'></a><br>Посчитаем? 4х4=","8","12","16","20",3],
+	],
+
+	data_array4_StarWr = [
+["<a><img src='images/StrWr1.jpg' alt='StWr'></a><br>Как называлась модель дроида, у которого было туловище в форме цилиндра и голова в форме полусферы?",
+"ТТ","АрЭнБи","R2D2","Серебряный",3],
+["<a><img src='images/SrWr-R2D2-120.jpg' alt='StWr'></a><br>Какой планеты не было в сериях 'Звёздных войн'?","Татуин","Корусант","Амикрон-Персей 8","Набу",3],
+["<a><img src='images/SrWr-Planets-120.jpg' alt='StWr'></a><br>Как звали большого мохнатого пилота-напарника Хана Соло?","Чуи",
+"Чубакка","Чучун","Артём",2],
+["<a><img src='images/SrWr-Chu-120.jpg' alt='StWr'></a><br>Кто говорил предложениями с неправильным порядком слов?","Оби-Ван","Магистр Энубин",
+"Магистр Йода","Килу Рен",3],
+["<a><img src='images/SrWr-Io-120.jpg' alt='StWr'></a><br>Какого цвета меч был у Дарта Вейдера?","Серый","Тёмно-серый","Красный","Оранжевый",3],
+["<a><img src='images/SrWr-W-120.jpg' alt='StWr'></a><br>Как звала Дарта Вейдера мама?","Сеня","Крепыш","Энакин","Супергонщик",3],
+["<a><img src='images/SrWr-En-120.jpg' alt='StWr'></a><br>Посчитаем? 7х7=","28","35","42","49",4],
+        ],
+
+	data_array5_Sam = [
+["<a><img src='images/Sam/Sam7-120.jpg' alt='Sam'></a><br>Как зовут пожарного с белыми усами в мультфильме 'Пожарный Сэм'?",
+"Офицер Каркасов","Томпсон","Старший лейтенант Стил", "Фёдор Фёдорович",3],
+["<a><img src='images/Sam/Sam4-120.jpg' alt='Sam'></a><br>Зачем пожарному топор?","Чтобы рубить дрова для костра","Чтобы варить кашу из топора","Чтобы устранять препятствия при тушении пожара","Чтобы бросать его в мишень надосуге",3],
+["<a><img src='images/Sam/Sam5-120.jpg' alt='Sam'></a><br>Что такое 'Краги'?","Сапоги пожарного",
+"Большие перчатки пожарного","Специальные шапки пожарного","Так пожарные называют языки пламени",2],
+["<a><img src='images/Sam/Sam2-120.jpg' alt='Sam'></a><br>Какой расцветки пожарные машины?","Жёлтые с красными полосами","Жёлтые с жёлтыми полосами",
+"Полностью чёрные","Красные с белыми или жёлтыми полосами",4],
+["<a><img src='images/Sam/Sam3-120.jpg' alt='Sam'></a><br>Какая порода собаки считается пожарной?","Пудель","Ризеншнауцер","Далматин","Фокс-терьер",3],
+["<a><img src='images/Sam/Sam6-120.jpg' alt='Sam'></a><br>Как называется нашивка на форме пожарного?","Значок","Пришивка","Шеврон","Кругляшок",3],
+["<a><img src='images/Sam/Sam2-120.jpg' alt='Sam'></a><br>Посчитаем? 8х8=","40","48","56","64",4],
+        ],
+	
+		     ];    
+    
+/* Ind as NUM 
+var MIX_ARRAY = [];
+var ARR = DATA_ARRAY0;
+var randomIndFu = function(ARR) {
+	return Math.floor(Math.random() * ARR.length);
+};
+var RandomInd = randomIndFu(ARR);
+var i;
+for (i = 0; i < ARR.length; i++) {
+	while (MIX_ARRAY.indexOf(RandomInd) !== -1) {
+		RandomInd = randomIndFu(ARR);
+	}
+	MIX_ARRAY[i] = RandomInd;
+}
+MIX_ARRAY;
+*/
+
+
+
+
+/* Ind as ARRAY[] */
+
+var MIX_ARRAY = [];
+var ARR = DATA_ARRAY0;
+var randomIndFu = function(ARR) {
+	return Math.floor(Math.random() * ARR.length);
+};
+var RandomInd = ARR[randomIndFu(ARR)];
+var i;
+for (i = 0; i < ARR.length; i++) {
+	while (MIX_ARRAY.indexOf(RandomInd) !== -1) {
+		RandomInd = ARR[randomIndFu(ARR)];
+	}
+	MIX_ARRAY[i] = RandomInd;
+}
+
+// full MIX_ARRAY; ok
+
+var fullIndFu = function(MIX_ARRAY){
+    var j=0; while (j<MIX_ARRAY.length){return MIX_ARRAY[j]; j++;}};
+fullIndFu(MIX_ARRAY);
+
+
+//fKf
