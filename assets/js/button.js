968eca419e294688b99f3abf2f2d8a6384fd5240
manrokor <53918265+manrokor@users.noreply.github.com>
2019-10-09 18:49:45 +0300

    Add files via upload

---
+++ assets/js/button.js
@@ -0,0 +1,19 @@
+
+// Mix array
+function playAudio3(){
+var myAudio = new Audio;
+myAudio.src = "audio/button/button3.mp3";
+myAudio.play();
+}
+            
+function playAudio2(){
+var myAudio = new Audio;
+myAudio.src = "audio/button/button2.mp3";
+myAudio.play();
+}
+
+function playAudio1(){
+var myAudio = new Audio;
+myAudio.src = "audio/button/button1.mp3";
+myAudio.play();
+}
